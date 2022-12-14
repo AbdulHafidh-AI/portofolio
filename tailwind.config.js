bc0fd320c9ce68a8de1b62c57cacb814e21b83e8
AbdulHafidh-AI <abdul_h20@mhs.unsyiah.ac.id>
2022-12-14 21:38:36 +0700

    add two section and modify some code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    container:{
+      center: true,
+      padding: '64px',
+    },
+    extend: {
+      colors:{
+        primary : "#34d399" ,// Emerald colour
+        secondary: "#94a3b8" // text-slate-400
+      }
+    },
   },
   plugins: [],
 }
